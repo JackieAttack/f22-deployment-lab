f57a2b17bd27affd4787cf4f036071a4986bef66
Jackie Bearnson <Jackie.bearnson@gmail.com>
2022-05-18 15:36:21 -0600

    Testing heroku

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use("/", express.static(path.join(__dirname, "../client")));
 
-
+app.get("/js", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/main.js"))
+})
 
 const port = process.env.PORT || 4005;
 
